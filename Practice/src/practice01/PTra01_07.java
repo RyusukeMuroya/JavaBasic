b4f499f35941c392a0811ff393f3199a9f1d13a5
DESKTOP-5MHI108\LIKEIT_STUDENT <ryusuke.muroya.likeit@gmail.com>
2018-10-10 19:05:03 +0900

    2-5
    1-2~2-5

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,9 +21,14 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
+		bo = true;
+		by = 1;
+		s  = 3;
+		c  ='Ⅿ';
+		i  = 7;
+		l  = 9;	
+		f  = 11;
+		d  = 13;
 
 
 
@@ -31,9 +36,14 @@ public class PTra01_07 {
 
 
 		// ★ それぞれの変数の中身を出力してください
-
-
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 
 
--- Practice/src/practice02/PTra02_01.java
+++ Practice/src/practice02/PTra02_01.java
@@ -10,12 +10,22 @@ public class PTra02_01 {
 	public static void main(String[] args) {
 
 		// ★ 20を5で割った値を出力してください
-
+		int a = 20; int b = 5;
+		int c = a/b;
+		
+		System.out.println("20を5で割った値は"+c);
 
 		// ★ 126に13を掛けた値を出力してください
+		int d  = 126;  int e  = 13;
+		int f  = d * e;
 
-
+		System.out.println("126に13を掛けた値は"+f);
+		
 		// ★ 97を6で割ったときの余りを出力してください
+		int g = 97;  int h = 6;
+		int i = g % h;
+		
+		System.out.println("97を6で割ったときの余りは"+i);
 
 
 	}
